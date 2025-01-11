d7ebd022d40f036f7a67d3bba822c0c2d957da44
ELizabeth <elizabethmites2703@gmail.com>
2025-01-11 13:55:05 -0500

    Examen

--- movies/models.py
+++ movies/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Movie(models.Model):
-    movie_id = models.AutoField(primary_key=True) 
-    title_movie = models.CharField(max_length=255)  
-    genero_movie = models.CharField(max_length=100)  
-    name = models.CharField(max_length=255)  
+    movie_id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    title_movie = models.CharField(max_length=30)  
+    genero_movie = models.CharField(max_length=30)  
+    name = models.CharField(max_length=500)  
     date = models.DateField(),
     synopsis = models.TextField()  
     def __str__(self):
